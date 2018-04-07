43527e05e91eaa3f76cca0facd83843713ccfc42
syeremy <syeremy@gmail.com>
2018-04-06 23:34:17 -0400

    working, but super slowwwww

---
+++ micro-nsbus6/myCode/Syeremy.Rest/Controller/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NServiceBus;
+using Syeremy.Messages;
+
+namespace Syeremy.Rest.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        private readonly IEndpointInstance _endpoint;
+
+        public HomeController(IEndpointInstance endpoint)
+        {
+            _endpoint = endpoint;
+        }
+        
+        // GET
+        public async Task<IActionResult> Index()
+        {
+            var processOrderCommand = new ProcessOrderCommand
+            {
+                AddressFrom = "My Home",
+                AddressTo = "Work",
+                Price =  12212,
+                Weight =  12
+            };
+            
+            await _endpoint.Send("elcavernas.queue.orders", processOrderCommand).ConfigureAwait(false);
+            
+            return  Content("processOrderCommand Sent!!!");
+        }
+    }
+}
