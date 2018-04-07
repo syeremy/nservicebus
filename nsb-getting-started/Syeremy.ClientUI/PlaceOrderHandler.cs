442b9a26a36b2d85571cbcfe6713db1fe76402e7
syeremy <syeremy@gmail.com>
2018-04-07 14:22:02 -0400

    Adding c# 7.2 support and doing nservicebus-gettingstarted - module 2

---
+++ nsb-getting-started/Syeremy.ClientUI/PlaceOrderHandler.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Logging;
+using Syeremy.Messages;
+
+namespace Syeremy.ClientUI
+{
+    public class PlaceOrderHandler : IHandleMessages<PlaceOrder>
+    {
+        static readonly ILog log = LogManager.GetLogger<PlaceOrderHandler>();
+
+        public async Task Handle(PlaceOrder message, IMessageHandlerContext context)
+        {
+            log.Info($"Received PlaceOrder, OrderId = {message.OrderId}");
+        }
+    }
+}
---
+++ nsb-getting-started/Syeremy.Messages/PlaceOrder.cs
@@ -0,0 +1,7 @@
+﻿namespace Syeremy.Messages
+{
+    public class PlaceOrder
+    {
+        
+    }
+}
