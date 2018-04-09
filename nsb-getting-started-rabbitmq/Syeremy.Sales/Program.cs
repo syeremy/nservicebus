876711873b72c96403fae5d907f3b8d0b652992c
syeremy <syeremy@gmail.com>
2018-04-09 01:39:20 -0400

    Start nsb-rabbitmq integration

---
+++ nsb-getting-started-rabbitmq/Syeremy.Sales/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Syeremy.Sales
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Title = "Sales";
+
+            var endpointConfiguration = new EndpointConfiguration("Sales");
+            var transport = endpointConfiguration.UseTransport<LearningTransport>();
+            
+            var recoverability = endpointConfiguration.Recoverability();
+            recoverability.Immediate(
+                immediate =>
+                {
+                    immediate.NumberOfRetries(3);
+                });
+            
+            recoverability.Delayed(
+                delayed =>
+                {
+                    delayed.NumberOfRetries(2);
+                    delayed.TimeIncrease(TimeSpan.FromMinutes(5));
+                });
+
+            var endpointInstance = await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+
+            Console.WriteLine("Press Any key to Exit");
+            Console.ReadKey();
+
+            await endpointInstance.Stop().ConfigureAwait(false);
+            
+        }
+    }
+}
